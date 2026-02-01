69a2ced21ed2ba69590675e5ac9428edef979933
Donny <donnyc12@outlook.com>
2026-02-01 15:34:29 -0600

    Minor Fixes

--- rtl_utils/if/fifo_wr_if.sv
+++ rtl_utils/if/fifo_wr_if.sv
@@ -1,21 +1,13 @@
 interface fifo_wr_if #(
-  parameter DATA_W = 32
+    parameter int DATA_W = 32
 );
+  logic [DATA_W-1:0] data;
+  logic              wr_en;
+  logic              full;
 
-  logic [DATA_W-1:0]  data;
-  logic               wr_en;
-  logic               full;
+  modport slave(input wr_en, input data, output full);
 
-  modport slave (
-    input   wr_en,
-    input   data,
-    output  full
-  );
-
-  modport master (
-    output  wr_en,
-    output  data,
-    input   full 
-  );
+  modport master(output wr_en, output data, input full);
 
 endinterface
+
