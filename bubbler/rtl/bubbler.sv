5f9a226c7b358f8735c6a76d2bfe9106fb7fdfd2
doc4science <doc4science@gmail.com>
2026-03-01 21:46:59 -0600

    fix the bubbler

--- bubbler/rtl/bubbler.sv
+++ bubbler/rtl/bubbler.sv
@@ -1,52 +1,54 @@
 module bubbler #(
-    parameter BIT_WIDTH_IN   = 64,
-    parameter BIT_WIDTH_OUT  = 66
+    parameter BIT_IN_W   = 64,
+    parameter BIT_OUT_W  = 66
 )(
-    input logic                              clock,
-    input logic                              reset,
-    input logic [BIT_WIDTH_IN-1:0]           input_64b,
-    input logic                              input_valid,
-    output logic [BIT_WIDTH_OUT-1:0]         output_66b,
-    output logic                             output_valid
+    input logic                              clk,
+    input logic                              rst,
+    input logic [BIT_IN_W-1:0]               _64b_i,
+    input logic                              valid_i,
+    output logic [BIT_OUT_W-1:0]             _66b_o,
+    output logic                             valid_o
 );
 
-logic [BIT_WIDTH_OUT-1:0]           remainder_c, remainder_q;
-logic [$clog2(BIT_WIDTH_OUT)-1:0]   bits_remaining_c, bits_remaining_q;
-logic [BIT_WIDTH_OUT-1:0]           output_c, output_q;
-logic                               valid_c, valid_q;
+localparam BIT_OUT_COUNT_W = $clog2(BIT_OUT_W);
 
-always_comb begin
-    remainder_c      = '0;
-    bits_remaining_c = '0;
-    output_c         = '0;
-    valid_c          = '0;
-    if ((bits_remaining_q + 64) < 66 || ~input_valid) begin
-        remainder_c      = input_64b;
-        bits_remaining_c = 64;
+logic [BIT_OUT_W-1:0]           remainder_d, remainder_q;
+logic [BIT_OUT_COUNT_W-1:0]     bits_remaining_d, bits_remaining_q;
+logic [BIT_OUT_W-1:0]           output_d, output_q;
+logic                           valid_d, valid_q;
+
+always_domb begin
+    remainder_d      = '0;
+    bits_remaining_d = '0;
+    output_d         = '0;
+    valid_d          = '0;
+    if ((bits_remaining_q + BIT_IN_W) < BIT_OUT_W || ~valid_i) begin
+        remainder_d      = _64b_i;
+        bits_remaining_d = BIT_IN_W;
     end else begin
-        output_c         = (66'(input_64b << bits_remaining_q)) & 66'h3FFFFFFFFFFFFFFFF;
-        output_c         = output_c + remainder_q;
-        remainder_c      = (66'(input_64b >> (64 - bits_remaining_q))) & 66'h3FFFFFFFFFFFFFFFF;
-        bits_remaining_c = bits_remaining_q - 2;
-        valid_c = 1'b1;
+        output_d         = (66'(_64b_i) << bits_remaining_q) & 66'h3FFFFFFFFFFFFFFFF;
+        output_d         = output_d + remainder_q;
+        remainder_d      = (66'(_64b_i) >> (BIT_IN_W - bits_remaining_q)) & 66'h3FFFFFFFFFFFFFFFF;
+        bits_remaining_d = bits_remaining_q - (BIT_OUT_W - BIT_IN_W);
+        valid_d          = 1'b1;
     end
 end
 
-always_ff @(posedge clock or posedge reset) begin
-    if(reset == 1'b1)begin
+always_ff @(posedge clk) begin
+    if(rst == 1'b1)begin
         output_q         <= '0;
         remainder_q      <= '0;
         bits_remaining_q <= '0;
         valid_q          <= '0;
     end else begin
-        valid_q          <= valid_c;
-        bits_remaining_q <= bits_remaining_c;
-        remainder_q      <= remainder_c;
-        output_q         <= output_c;
+        valid_q          <= valid_d;
+        bits_remaining_q <= bits_remaining_d;
+        remainder_q      <= remainder_d;
+        output_q         <= output_d;
     end
 end
 
-assign output_66b                   = output_q;
-assign output_valid                 = valid_q;
+assign _66b_o  = output_q;
+assign valid_o = valid_q;
 
 endmodule
