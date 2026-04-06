92d7ea39017ce6796de0f83b59aaabfcd636986b
Zirun Mao <esf2634@moore.wot.ece.northwestern.edu>
2026-04-06 16:51:08 -0500

    chore: align branch to main outside tx_module

---
+++ median_filter/rtl/median_filter_pkg.sv
@@ -0,0 +1,14 @@
+package median_filter_pkg;
+
+  localparam int KERNEL_LEN = 2;
+  localparam int PIXEL_W    = 8;
+
+  typedef struct {
+    logic [PIXEL_W-1:0] red;
+    logic [PIXEL_W-1:0] green;
+    logic [PIXEL_W-1:0] blue;
+  } pixel_t;
+
+  // Add more to package down here
+
+endpackage
