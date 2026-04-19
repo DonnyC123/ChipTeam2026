a5502b387d31761c2d016d32bfdf14f5ec7540a0
Jacob J <doublej7565@gmail.com>
2026-04-19 16:02:28 -0500

    Fixed some off by 1 errors

--- pcs_generator/rtl/pcs_generator.sv
+++ pcs_generator/rtl/pcs_generator.sv
@@ -10,6 +10,7 @@
 //     
 //   There is a minimum IPG of 12 bytes between a an 'end' -> next 'start'
 //   The C lanes after T do count toward the interpacket gap
+//   25G ethernt packets have a minimum of 64 bytes
 
 import pcs_pkg::*;
 
@@ -31,7 +32,6 @@ skid_entry_t              skid_value_d;
 leftover_bytes_t          leftover_bytes_q;
 leftover_bytes_t          leftover_bytes_d;
 
-
 typedef enum logic [3:0] {WAIT_START, DATA, EOF, IDLE_OUT} state_t;
 state_t current_state, next_state;
 
@@ -41,7 +41,6 @@ logic [2:0] held_byte_cnt_d, held_byte_cnt_q; //max ever stored = 5
 logic [3:0] num_incoming_d, num_incoming_q; //max ever incoming = 8
 
 // Clocked port outputs
-logic                 ready_d;
 logic                 get_axi;
 logic [CONTROL_W-1:0] out_control_d;
 logic [DATA_W-1:0]    out_data_d;
@@ -57,8 +56,8 @@ assign get_axi      = axis_slave_if.tvalid && axis_slave_if.tready;
 always_comb begin
     skid_value_d.data             = axis_slave_if.tdata; //we will put the data in network order here, if we switch up again
     skid_value_d.valid_bytes_mask = axis_slave_if.tkeep;
-    skid_value_d.last_byte        = last_byte_i;
-    skid_value_d.valid_data_i     = axis_slave_if.valid_data_i;
+    skid_value_d.last_byte        = axis_slave_if.tlast;
+    skid_value_d.valid_data_i     = axis_slave_if.tvalid;
 end
 
 // FSM combinational block
@@ -109,7 +108,7 @@ always_comb begin
                 if(held_byte_cnt_q == 3'd5) begin //the num of bytes stored will never change as long as valid_mask = '1
                     out_data_d = {skid_value_q.data[(BYTE_W*3)-1:0], leftover_bytes_q};
                 end else begin 
-                    out_data_d = {skid_value_q.data[(BYTE_W*7)-1:0], leftover_bytes_q[LEFTOVER_T_W -: BYTE_W]};
+                    out_data_d = {skid_value_q.data[(BYTE_W*7)-1:0], leftover_bytes_q[LEFTOVER_T_W-1 -: BYTE_W]};
                 end
 
                 out_valid_d = 1'b1; 
@@ -131,11 +130,11 @@ always_comb begin
                         case(num_incoming_q)
                             3'd0 : out_data_d = {{7{8'd0}}, TERM_L0};
 
-                            3'd5 : out_data_d = {{2{8'd0}}, leftover_bytes_q[LEFTOVER_T_W -: BYTE_W*5], TERM_L5};
+                            3'd5 : out_data_d = {{2{8'd0}}, leftover_bytes_q[LEFTOVER_T_W-1 -: BYTE_W*5], TERM_L5};
                 
-                            3'd6 : out_data_d = {{1{8'd0}}, skid_value_q.data[0 +: BYTE_W-1], leftover_bytes_q[LEFTOVER_T_W -: BYTE_W*5], TERM_L6};
+                            3'd6 : out_data_d = {{1{8'd0}}, skid_value_q.data[0 +: BYTE_W-1], leftover_bytes_q[LEFTOVER_T_W-1 -: BYTE_W*5], TERM_L6};
                 
-                            3'd7 : out_data_d = {skid_value_q.data[0 +: (BYTE_W*2)-1], leftover_bytes_q[LEFTOVER_T_W -: BYTE_W*5], TERM_L6};
+                            3'd7 : out_data_d = {skid_value_q.data[0 +: (BYTE_W*2)-1], leftover_bytes_q[LEFTOVER_T_W-1 -: BYTE_W*5], TERM_L6};
                         endcase
                     end else begin
                         // this case pulls 1 and then num_in should be between 0-6
@@ -143,25 +142,25 @@ always_comb begin
                         case(num_incoming_q)
                             3'd0 : out_data_d = {{7{8'd0}}, TERM_L0};
 
-                            3'd1 : out_data_d = {{6{8'd0}}, leftover_bytes_q[LEFTOVER_T_W -: BYTE_W], TERM_L1};
+                            3'd1 : out_data_d = {{6{8'd0}}, leftover_bytes_q[LEFTOVER_T_W-1 -: BYTE_W], TERM_L1};
                 
-                            3'd2 : out_data_d = {{5{8'd0}}, skid_value_q.data[0 +: (BYTE_W*1)-1], leftover_bytes_q[LEFTOVER_T_W -: BYTE_W], TERM_L2};
+                            3'd2 : out_data_d = {{5{8'd0}}, skid_value_q.data[0 +: (BYTE_W*1)-1], leftover_bytes_q[LEFTOVER_T_W-1 -: BYTE_W], TERM_L2};
                 
-                            3'd3 : out_data_d = {{4{8'd0}}, skid_value_q.data[0 +: (BYTE_W*2)-1], leftover_bytes_q[LEFTOVER_T_W -: BYTE_W], TERM_L3};
+                            3'd3 : out_data_d = {{4{8'd0}}, skid_value_q.data[0 +: (BYTE_W*2)-1], leftover_bytes_q[LEFTOVER_T_W-1 -: BYTE_W], TERM_L3};
                 
-                            3'd4 : out_data_d = {{3{8'd0}}, skid_value_q.data[0 +: (BYTE_W*3)-1], leftover_bytes_q[LEFTOVER_T_W -: BYTE_W], TERM_L4};
+                            3'd4 : out_data_d = {{3{8'd0}}, skid_value_q.data[0 +: (BYTE_W*3)-1], leftover_bytes_q[LEFTOVER_T_W-1 -: BYTE_W], TERM_L4};
                 
-                            3'd5 : out_data_d = {{2{8'd0}}, skid_value_q.data[0 +: (BYTE_W*4)-1], leftover_bytes_q[LEFTOVER_T_W -: BYTE_W], TERM_L5};
+                            3'd5 : out_data_d = {{2{8'd0}}, skid_value_q.data[0 +: (BYTE_W*4)-1], leftover_bytes_q[LEFTOVER_T_W-1 -: BYTE_W], TERM_L5};
                 
-                            3'd6 : out_data_d = {{1{8'd0}}, skid_value_q.data[0 +: (BYTE_W*5)-1], leftover_bytes_q[LEFTOVER_T_W -: BYTE_W], TERM_L6};
+                            3'd6 : out_data_d = {{1{8'd0}}, skid_value_q.data[0 +: (BYTE_W*5)-1], leftover_bytes_q[LEFTOVER_T_W-1 -: BYTE_W], TERM_L6};
                 
-                            3'd7 : out_data_d = {skid_value_q.data[0 +: (BYTE_W*6)-1],  leftover_bytes_q[LEFTOVER_T_W -: BYTE_W], TERM_L7};
+                            3'd7 : out_data_d = {skid_value_q.data[0 +: (BYTE_W*6)-1],  leftover_bytes_q[LEFTOVER_T_W-1 -: BYTE_W], TERM_L7};
                         endcase
 
                     end
 
                     out_valid_d     = 1'b1;
-                    held_byte_cnt_q = 3'd0;
+                    held_byte_cnt_d = 3'd0;
                 end else begin // if num_incoming_q + held_byte_cnt_q > 7, we need to output another data packet before flushing
                     out_control_d    = DATA_HDR;
                     tready_d         = 1'b0; //We don't need another AXI beat right now since we are forcing a data_frame
@@ -185,7 +184,7 @@ always_comb begin
             out_control_d    = CTRL_HDR;
             out_data_d       = {{7{8'd0}}, IDLE_BLK};
             out_valid_d      = 1'b1;
-            leftover_bytes_q = (get_axi) ?  3'd5 : 3'd1; //if data right away need SOF_L4, else SOF_L0
+            leftover_bytes_d = (get_axi) ?  3'd5 : 3'd1; //if data right away need SOF_L4, else SOF_L0
         end
         
         default : begin
@@ -202,13 +201,14 @@ always_ff@(posedge clk)begin
         axis_slave_if.tready <= '0;
         out_valid_o          <= '0;
         held_byte_cnt_q      <= '0;
+        leftover_bytes_q     <= '0;
         num_incoming_q       <= '0;
     end else begin
         current_state <= next_state;
         if(get_axi == 1'b1) begin
             skid_value_q <= skid_value_d;
         end
-        axis_slave_if.tready <= ready_d;
+        axis_slave_if.tready <= tready_d;
         held_byte_cnt_q      <= held_byte_cnt_d;
         out_control_o        <= out_control_d;
         out_data_o           <= out_data_d;
