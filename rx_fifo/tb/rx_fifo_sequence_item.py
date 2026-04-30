2adc9ee432ead8dd4d3625f1511fc3b560db43e4
Jacob J <doublej7565@gmail.com>
2026-04-30 14:46:46 -0500

    updated sequence item w/ new ports/names

--- rx_fifo/tb/rx_fifo_sequence_item.py
+++ rx_fifo/tb/rx_fifo_sequence_item.py
@@ -4,6 +4,12 @@ from typing import Any, Dict, Self
 from cocotb.types import Logic, LogicArray
 
 from tb_utils.abstract_transactions import AbstractTransaction
+#TODO: in clock = 403Mhz, out clock = 250MHz
+
+@dataclass
+class AXIStreamReadyTransaction:
+    ready: Logic = field(default_factory=lambda: Logic("1"))
+
 
 @dataclass
 class RXFifoSequenceItem(AbstractTransaction):
@@ -20,8 +26,15 @@ class RXFifoSequenceItem(AbstractTransaction):
         default_factory=lambda: LogicArray("X" * RXFifoSequenceItem.IN_MASK_W)
     )
     valid_i: Logic = field(default_factory=lambda: Logic("0"))
+    s_clk: Logic = field(default_factory=lambda: Logic("0"))
+    m_clk: Logic = field(default_factory=lambda: Logic("0"))
+    rst: Logic = field(default_factory=lambda: Logic("0"))
+
     drop_i: Logic = field(default_factory=lambda: Logic("0"))
     send_i: Logic = field(default_factory=lambda: Logic("0"))
+    m_axi: AXIStreamReadyTransaction = field(
+        default_factory=AXIStreamReadyTransaction
+    )
 
     @staticmethod
     def _to_int(value: Any, default: int = 0) -> int:
@@ -35,9 +48,13 @@ class RXFifoSequenceItem(AbstractTransaction):
         return cls(
             data_i=LogicArray("X" * cls.DATA_IN_W),
             mask_i=LogicArray("X" * cls.IN_MASK_W),
+            s_clk=Logic(0),
+            m_clk=Logic(0),
+            rst=Logic(0),
             valid_i=Logic(0),
             drop_i=Logic(0),
             send_i=Logic(0),
+            m_axi=AXIStreamReadyTransaction(ready=Logic(0)),
         )
 
     @property
@@ -51,9 +68,15 @@ class RXFifoSequenceItem(AbstractTransaction):
     @property
     def to_data(self) -> Dict[str, Any]:
         return {
+            "s_clk": bool(self._to_int(self.s_clk, 0)),
+            "m_clk": bool(self._to_int(self.m_clk, 0)),
+            "rst": bool(self._to_int(self.rst, 0)),
             "data_i": self._to_int(self.data_i, 0),
             "mask_i": self._to_int(self.mask_i, 0),
             "valid_i": bool(self._to_int(self.valid_i, 0)),
             "drop_i": bool(self._to_int(self.drop_i, 0)),
             "send_i": bool(self._to_int(self.send_i, 0)),
+            "m_axi": {
+            "ready": bool(self._to_int(self.m_axi.ready, 0)),
+            },
         }
